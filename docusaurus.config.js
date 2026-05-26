6aeaa51ef79aaa87cd29540c776f96a6e9129d7e
Rajith <rrajith660@gmail.com>
2026-05-26 11:59:14 +0530

    update the items from browse docs to OMS

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: '📚 Browse Docs',
+            label: 'OMS',
           },
        ], 
       },
