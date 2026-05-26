5dfb582773f69a0cda4385bddfcd319f478ea619
Rajith <rrajith660@gmail.com>
2026-05-26 11:32:01 +0530

    Added custom mobile sidebar

---
+++ src/theme/DocSidebar/Mobile/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import DocSidebarMobile from '@theme-original/DocSidebar/Mobile';
+
+export default function DocSidebarMobileWrapper(props) {
+  return <DocSidebarMobile {...props} />;
+}
